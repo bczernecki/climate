8f07aebce4e56483e801ec2fe91b295019e8e891
Nowosad <tupiszakaczki@gmail.com>
2019-07-18 13:09:05 +0200

    adds ogimet tests

---
+++ tests/testthat/test-meteo_ogimet.R
@@ -0,0 +1,19 @@
+context("meteo_ogimet")
+y <-  2018
+
+test_that("meteo_ogimet works!", {
+  x2 <- meteo_ogimet(interval = "hourly", date = c("2019-06-01", "2019-07-08"),
+                     station = 12330, coords = TRUE, precip_split = TRUE)
+  
+  x <- meteo_ogimet(interval = "hourly", date = c("2019-06-01", "2019-07-08"),
+                    station = 12330, coords = TRUE, precip_split = FALSE)
+  
+  x <- meteo_ogimet(interval = "daily", date = c("2019-06-01", "2019-07-08"),
+                    station = 12330, coords = TRUE, precip_split = TRUE)
+  
+  x <- meteo_ogimet(interval = "daily", date = c("2019-06-01", "2019-07-08"),
+                    station = 12330, coords = FALSE, precip_split = TRUE)
+})
+
+
+
