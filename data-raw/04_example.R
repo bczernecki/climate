94fea066a40e00df35586e48ceca61382c9aa11f
unknown <arkadiusz.glogowski@upwr.edu.pl>
2019-11-06 14:48:12 +0100

    add examples form papers

---
+++ data-raw/04_example.R
@@ -0,0 +1,6 @@
+library(climate)
+profile <- sounding_wyoming(wmo_id = 12120,yy = 2019, mm = 4, dd = 4, hh = 0)
+df <- profile[[1]]
+colnames(df)[c(1, 3:4)] = c("press", "temp", "dewpt") # convert column names
+RadioSonde::plotsonde(df, winds = FALSE, title = "2019-04-04 00UTC (LEBA, PL)",
+                      col = c("red", "blue"), lwd = 3)
---
+++ data-raw/05_example.R
@@ -0,0 +1,31 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(sf)
+library(tmap)
+library(climate)
+#downloading data:
+y <- meteo_imgw("monthly", "synop", year = 1978:2017, coords = TRUE)
+y%>%
+  filter(!(mm>5 && mm<9 && t2m_mean_mon == 0))%>% 
+  select(station, X, Y, yy, mm, t2m_mean_mon)%>%
+  group_by(station, yy, X, Y)%>%
+  summarise(annual_mean_t2m = mean(t2m_mean_mon), n = n())%>%
+  filter(n == 12)%>%
+  spread(yy, annual_mean_t2m)%>%
+  na.omit() -> trend
+
+regression <- function(x) {
+  df<- data.frame(yy = 1978:2017, temp=as.numeric(x))
+  coef(lm(temp ~ yy, data = df))[2]
+}
+trend$coef <- round(apply(trend[, -1:-4], 1, regression) * 100, 1)
+trend <- st_as_sf(trend, coords = c("X","Y"), crs=4326)
+
+library(rnaturalearth)
+library(rnaturalearthdata)
+world <- ne_countries(scale = "medium", returnclass = "sf")
+tm <- tm_shape(world) + tm_borders()+
+  tm_shape(trend, is.master = TRUE) + tm_dots(col = "coef", size = 4)+
+  tm_shape(trend) + tm_text(text = "coef")
+tm
