ec13c12587d09b0d47a26c0bc87dc40cd38a5ab2
unknown <arkadiusz.glogowski@upwr.edu.pl>
2019-11-11 19:46:44 +0100

    better names of arguments

--- data-raw/03_example.R
+++ data-raw/03_example.R
@@ -1,6 +1,10 @@
-library(remotes)
-install_github("bczernecki/climate")
 library(climate)
-x <- meteo_ogimet(interval = "daily", date = c("2019-06-01", "2019-07-08"),
-                        station = c(12330, 22222), coords = FALSE)
-profile <- sounding_wyoming(wmo_id = 12220, yy = 2019, mm = 4, dd = 4, hh = 0)
+ns = nearest_stations_ogimet(point = c(-4, 56), no_of_stations = 50, add_map = TRUE)
+head(ns)
+#>    wmo_id       station_names       lon       lat alt  distance [km]
+#> 29  03144         Strathallan  -3.733348 56.31667  35      46.44794
+#> 32  03155           Drumalbin  -3.733348 55.61668 245      52.38975
+#> 30  03148           Glen Ogle  -4.316673 56.41667 564      58.71862
+#> 27  03134   Glasgow Bishopton  -4.533344 55.90002  59      60.88179
+#> 35  03166 Edinburgh Gogarbank  -3.350007 55.93335  57      73.30942
+#> 28  03136      Prestwick RNAS  -4.583345 55.51668  26      84.99537
