db2369648c6a6dcc72a495a9b9fd734e60073055
Nowosad <tupiszakaczki@gmail.com>
2019-11-25 11:15:55 +0100

    updates docs and tests

--- tests/testthat/test-nearest_stations_ogimet.R
+++ tests/testthat/test-nearest_stations_ogimet.R
@@ -1,16 +1,17 @@
 context("meteo_imgw")
 
 test_that("nearest_stations_ogimet works!", {
-  x <- nearest_stations_ogimet(country = "United+Kingdom", point = c(10, 50), add_map = FALSE, numbers_station = 10)
+  x <- nearest_stations_ogimet(country = "United+Kingdom", point = c(10, 50), add_map = FALSE, no_of_stations = 10)
   
-  x <- nearest_stations_ogimet(country = "United+Kingdom", point = c(10, 50), add_map = TRUE, numbers_station = 10)
+  x <- nearest_stations_ogimet(country = "United+Kingdom", point = c(10, 50), add_map = TRUE, no_of_stations = 10)
 
-  x <- nearest_stations_ogimet(country = "United+Kingdom", point = c(-10, -50), add_map = TRUE, numbers_station = 10)
+  x <- nearest_stations_ogimet(country = "United+Kingdom", point = c(-10, -50), add_map = TRUE, no_of_stations = 10)
   
-  x <- nearest_stations_ogimet(country = "Poland", point = c(10, 50), add_map = TRUE, numbers_station = 10)
+  x <- nearest_stations_ogimet(country = "Poland", point = c(10, 50), add_map = TRUE, no_of_stations = 10)
   
-  x <- nearest_stations_ogimet(country = "Pland", point = c(10, 50), add_map = TRUE, numbers_station = 10)
+  # expected error
+  x <- nearest_stations_ogimet(country = "Pland", point = c(10, 50), add_map = TRUE, no_of_stations = 10)
   
-  x <- nearest_stations_ogimet(country = c("United+Kingdom", "Poland"), point = c(0, 0), add_map = TRUE, numbers_station = 150)
+  x <- nearest_stations_ogimet(country = c("United+Kingdom", "Poland"), point = c(0, 0), add_map = TRUE, no_of_stations = 150)
   
 })
