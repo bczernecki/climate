11202b57d0622223a1baceb96546bde2fe9d265b
bczernecki <nwp@amu.edu.pl>
2020-05-19 21:26:34 +0200

    typos

--- R/nearest_stations_noaa.R
+++ R/nearest_stations_noaa.R
@@ -24,10 +24,15 @@
 #' }
 #'
 
-nearest_stations_nooa <- function(country = NULL, 
+nearest_stations_nooa <- function(country,
                                   date = Sys.Date(), 
                                   add_map = TRUE, point = NULL, 
-                                  no_of_stations = 5, ...){
+                                  no_of_stations = 100, ...){
+  
+  if(missing(country) | is.null(country)){
+    stop("No country provided!")
+  }
+  
   if (length(point)>2) {
     stop("Too many points for the distance calculations. Please provide just one point")
   } else if (length(point)<2) {
@@ -45,34 +50,34 @@ nearest_stations_nooa <- function(country = NULL,
   temp = tempfile()
   test_url(link = linkpl2, output = temp)
   a = readLines(temp)
-  a = trimws(a,which = "right")
-  b <- strsplit(a, "          ")
-  b1=do.call(rbind, b)
-  colnames(b1)=c("CTRY","countries")
-  b1=as.data.frame(b1[2:dim(b1)[1],])
-  b1$CTRY=as.character(b1$CTRY)
-  b1$countries=as.character(b1$countries)
-  b2<-read.csv("https://www1.ncdc.noaa.gov/pub/data/noaa/isd-history.csv")
-  stations_noaa=merge(b1,b2)
-  stations_noaa["Begin_date"]=as.Date(paste0(substr(stations_noaa[,11],1,4),"-",substr(stations_noaa[,11],5,6),"-",substr(stations_noaa[,11],7,8)))
-  stations_noaa["End_date"]=as.Date(paste0(substr(stations_noaa[,12],1,4),"-",substr(stations_noaa[,12],5,6),"-",substr(stations_noaa[,12],7,8)))
-  result=stations_noaa
- 
+  a = trimws(a, which = "right")
+  b = strsplit(a, "          ")
+  b1 = do.call(rbind, b)
+  colnames(b1) = c("CTRY","countries")
+  b1 = as.data.frame(b1[2:dim(b1)[1],])
+  b1$CTRY = as.character(b1$CTRY)
+  b1$countries = as.character(b1$countries)
+  b2 = read.csv("https://www1.ncdc.noaa.gov/pub/data/noaa/isd-history.csv")
+  stations_noaa = merge(b1,b2)
+  stations_noaa["Begin_date"] = as.Date(paste0(substr(stations_noaa[,11],1,4),"-",substr(stations_noaa[,11],5,6),"-",substr(stations_noaa[,11],7,8)))
+  stations_noaa["End_date"] = as.Date(paste0(substr(stations_noaa[,12],1,4),"-",substr(stations_noaa[,12],5,6),"-",substr(stations_noaa[,12],7,8)))
+  result = stations_noaa
+  
   if (!is.null(country)){
     result=result[result$countries==country,]
   }
   
   if (dim(result)[1]==0) {
-    stop("Wrong name of a country. Please check countries names at 
+    stop("Wrong name of a country. Please check countries names at: 
          https://www1.ncdc.noaa.gov/pub/data/noaa/country-list.txt")
   } 
   result=result[(result$Begin_date<date & result$End_date<date), ]
   if (dim(result)[1]==0) {
-    stop("Propobly there is no data for this date. Please check available records :  
+    stop("Propobly there is no data for this date. Please check available records:  
         https://www1.ncdc.noaa.gov/pub/data/noaa/isd-history.txt")
   } 
   if (is.null(point)){
-    point=c(round(mean(result$LON,na.rm=T),2),round(mean(result$LAT,na.rm=T),2))
+    point = c(round(mean(result$LON,na.rm=T),2),round(mean(result$LAT,na.rm=T),2))
   }
   point = as.data.frame(t(point))
   names(point) = c("LON", "LAT")
