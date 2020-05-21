918d7ab4920db4b3e3409164e7987078ba969684
bczernecki <nwp@amu.edu.pl>
2020-05-22 00:22:52 +0200

    onAttach

--- R/onAttach.R
+++ R/onAttach.R
@@ -2,8 +2,9 @@
 #' @export
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste0(c("\n************************\nWelcome to climate ", as.character(packageVersion("climate")), "!\n",
-                                 "Find out more about the package and data sources at: github.com/bczernecki/climate\n",
-                                 "Using climate as a data source for a publication? Please cite it: citation('climate')\n",  
+  ver = as.character(packageVersion("climate"))
+  packageStartupMessage(paste0(c("\n************************\nWelcome to climate ", ver, "!\n",
+                                 "\nFind out more about the package and data sources at: github.com/bczernecki/climate",
+                                 "\nUsing climate as a data source for a publication? Please cite it: citation('climate')\n",  
                                  "************************\n")))
 }
