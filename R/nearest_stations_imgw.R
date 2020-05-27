89ea54b8b66424cc3a69a473443be8d53309fadc
bczernecki <nwp@amu.edu.pl>
2020-05-27 12:15:33 +0200

    nearest_stations_imgw bug fixes

--- R/nearest_stations_imgw.R
+++ R/nearest_stations_imgw.R
@@ -1,14 +1,14 @@
-#' List of nearby meteorological or hydrological stations in Poland for a defined geographical location
+#' List of nearby meteorological or hydrological stations in Poland for a defined geographical coordinates
 #'
-#' Returns a data frame of meteorological or hydrological stations with their coordinates and distance from a given location based on the noaa website. 
-#' The returned list is valid only for a given day. 
+#' Returns a data frame of meteorological or hydrological stations with their coordinates in particular year. 
+#' The returned object is valid only for a given year and type of stations (e.g. "synop", "climate" or "precip"). 
 #'
 #' @param type data name;"meteo" (default), "hydro" 
-#' @param rank rank of the stations: "synop" (default), "climate", or "precip"
+#' @param rank rank of the stations: "synop" (default), "climate", or "precip"; Only valid if type = "meteo
 #' @param year select year for serching nearest station
 #' @param add_map logical - whether to draw a map for a returned data frame (requires maps/mapdata packages)
 #' @param point a vector of two coordinates (longitude, latitude) for a point we want to find nearest stations to (e.g. c(80, 6))
-#' @param no_of_stations how many nearest stations will be returned from the given geographical coordinates
+#' @param no_of_stations how many nearest stations will be returned from the given geographical coordinates. 50 used by default
 #' @param ... extra arguments to be provided to the [graphics::plot()] function (only if add_map = TRUE)
 #' @importFrom XML readHTMLTable
 #' @export
@@ -26,38 +26,46 @@
 #' }
 #'
 
-nearest_stations_imgw <- function(type="meteo", 
-                                  rank="synop",
-                                  year=2018,
-                                  add_map = TRUE, point = NULL, 
-                                  no_of_stations = 5, ...){
+nearest_stations_imgw <- function(type = "meteo", 
+                                  rank = "synop",
+                                  year = 2018,
+                                  add_map = TRUE, 
+                                  point = NULL, 
+                                  no_of_stations = 50, ...){
   if (length(point)>2) {
-    stop("Too many points for the distance calculations. Please provide just one point")
-  } else if (length(point)<2) {
-    message("The point should have two coordinates. \n We will provide nearest stations for mean location. \n To change it please change the `point` argument c(LON,LAT)" )
+    stop("Too many points for the distance calculations. Please provide just one pair of coordinates (e.g. point = c(17,53))")
+  } else if (length(point)<2 | length(point) == 0) {
+    message("
+            The point argument should have two coordinates.
+            We will provide nearest stations for mean location of all available stations.
+            To change it please change the `point` argument c(LON,LAT)" )
+    Sys.sleep(2)
   }
   
   
   if (max(year)>=as.integer(substr(Sys.Date(),1,4))-1) {
-    message("Data can be not provided to the repository, please check the availablity at: \n
+    message("Data cannot be provided for this repository. Please check the available records at: \n
             https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/")
   }
   
-  if (type=="meteo"){
-    result=unique(meteo_imgw_monthly(rank = rank,year = year,coords = T)[,c(2:5)])
+  if (type == "meteo"){
+    result = unique(meteo_imgw_monthly(rank = rank, year = year, coords = T)[,c(2:5)])
   } else if (type=="hydro"){
-    result=unique(hydro_imgw_annual(year = year,coords = T)[,c(1:4)])
+    result = unique(hydro_imgw_annual(year = year, coords = T)[,c(1:4)])
   } else {
-    stop("You provide wrong type of imgw data please provide \"meteo\", or \"hydro\"")
+    stop("You've provided wrong type argument; please use: \"meteo\", or \"hydro\"")
   }
 
   if (dim(result)[1]==0) {
-    stop("Propobly there is no data. Please check available records :  
+    stop("Propobly there is no data in the downloaded object. Please check available records:  
         https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/")
   } 
   if (is.null(point)){
-    point=c(round(mean(result$LON,na.rm=T),2),round(mean(result$LAT,na.rm=T),2))
+  # workaround for different column names:
+    if(any(colnames(result)=="LON")) point = c(round(mean(result$LON,na.rm=T),2),round(mean(result$LAT,na.rm=T),2))
+    if(any(colnames(result)=="X")) point = c(round(mean(result$X,na.rm=T),2),round(mean(result$Y,na.rm=T),2))
   }
+  
   
   point = as.data.frame(t(point))
   names(point) = c("X", "Y")
@@ -76,7 +84,7 @@ nearest_stations_imgw <- function(type="meteo",
     if (!requireNamespace("maps", quietly = TRUE)){
       stop("package maps required, please install it first")
     }
-    # plot labels a little bit higher...
+    # plot a little bit more:
     addfactor <- as.numeric(diff(stats::quantile(result$Y, na.rm = TRUE, c(0.48, 0.51)))) #lat Y
     addfactor <- ifelse(addfactor > 0.2, 0.2, addfactor)
     addfactor <- ifelse(addfactor < 0.05, 0.05, addfactor)
@@ -88,16 +96,9 @@ nearest_stations_imgw <- function(type="meteo",
       pch = 19,
       xlab = "longitude",
       ylab = "latitude",
-      xlim = (c(min(
-        c(result$X, point$X)
-      ) - 3, max(
-        c(result$X, point$X)
-      ) + 3)),
-      ylim = (c(min(
-        c(result$Y, point$Y)
-      ) - 3, max(
-        c(result$Y, point$Y)
-      ) + 3)), ...
+      xlim = (c(min(c(result$X, point$X), na.rm = T) - 1, max(c(result$X, point$X),na.rm = T) + 1)),
+      ylim = (c(min(c(result$Y, point$Y), na.rm = T) - 1, max(c(result$Y, point$Y),na.rm = T) + 1)),
+      ...
     )
     graphics::points(
       x = point[1],
@@ -117,7 +118,7 @@ nearest_stations_imgw <- function(type="meteo",
     
   }
   if (length(year)>1) {
-    message("Please provide only one year, for more years station may change names and duplicate")
+    message("Please provide only one year. For more years station's metadata may change (name, location or station may stop collecting data)")
   }
   return(result)
 }
