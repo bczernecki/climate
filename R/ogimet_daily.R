4281c9e502aeab4e71fbef0af8f9766010796343
bczernecki <nwp@amu.edu.pl>
2020-07-30 16:42:12 +0200

    ogimet fixes

--- R/ogimet_daily.R
+++ R/ogimet_daily.R
@@ -82,146 +82,142 @@ ogimet_daily <- function(date = c(Sys.Date()-30, Sys.Date()), coords = FALSE, st
       linkpl2 <- paste("https://www.ogimet.com/cgi-bin/gsynres?lang=en&ind=", station_nr, "&ndays=32&ano=", year, "&mes=", month, "&day=", day, "&hora=", hour,"&ord=REV&Send=Send", sep="")
       if(month == 1) linkpl2 <- paste("https://www.ogimet.com/cgi-bin/gsynres?lang=en&ind=", station_nr, "&ndays=32&ano=", year, "&mes=", month, "&day=", day, "&hora=", hour, "&ord=REV&Send=Send", sep="")
       
-      # if (!httr::http_error(linkpl2)) {
-      #   a = getURL(linkpl2,
-      #              ftp.use.epsv = FALSE,
-      #              dirlistonly = TRUE)
-      # } else {
-      #   stop(call. = FALSE, 
-      #        paste0("\nDownload failed. ",
-      #               "Check your internet connection or validate this url in your browser: ",
-      #               linkpl2, "\n"))
-      # }
-      
       
       temp = tempfile()
       test_url(linkpl2, temp)
       
-      #a <- getURL(linkpl2)
-      a <- readHTMLTable(temp, stringsAsFactors = FALSE)
-      unlink(temp)
-      
-      b <- a[[length(a)]]
-      if (sum(b[1,]=="Dailyweather summary", na.rm = TRUE)) {
-        b <- b[,1:(length(b) - 8)]
-      } else {
-        b <- b[, 1:length(b)]
-      }
-      
-      test <- b[1:2, ]
-      if (is.null(test)) {
-        warning(paste0("Wrong station ID: ", station_nr, " You can check station ID at https://ogimet.com/display_stations.php?lang=en&tipo=AND&isyn=&oaci=&nombre=&estado=&Send=Send"))
-        return(data_station)
-        } 
-      #exeptions diferent names_col
-        if ((length(test[2, !is.na(test[2, ])]) == 6 &
-             test[2, 5] == "Int.")) {
-          names_col = unlist(c(
-            test[1, 1],
-            paste(test[1, 2], test[2, 1:3], sep = "_"),
-            test[1, 3:4],
-            paste(test[1, 5], test[2, 4:6], sep = "_"),
-            test[1, c(6:(length(test) - 4))]
-          ))
-        } else if ((length(test[2, !is.na(test[2, ])]) == 2 &
-                    test[2, 2] == "Int.")) {
-          names_col = unlist(c(test[1, 1:2], paste(test[1, 3], test[2, 1:2], sep = "_"), test[1, c(4:(length(test) - 1))]))
-        } else if ((length(test[2, !is.na(test[2, ])]) == 5 &
-                    test[2, 5] == "Int.")) {
-          names_col = unlist(c(
-            test[1, 1],
-            paste(test[1, 2], test[2, 1:3], sep = "_"),
-            test[1, 3:4],
-            paste(test[1, 5], test[2, 4:5], sep = "_"),
-            test[1, c(6:(length(test) - 3))]
-          ))
-        } else if ((length(test[2, !is.na(test[2, ])]) == 3 &
-                    test[2, 2] == "Int.")) {
-          names_col = unlist(c(
-            test[1, 1:2],
-            paste(test[1, 3], test[2, 1:3], sep = "_"),
-            test[1, c(4:(length(test) - 2))]
-          ))
-        }else {
-          names_col = "Error_column"
-        }
-      
-      
-      names_col <-
-        gsub("[^A-Za-z0-9]",
-             "",
-             as.character(lapply(names_col, as.character), stringsAsFactors = FALSE))
-
-      colnames(b) <- names_col
-      b <- b[-c(1:2), ]
-      b["station_ID"] <- station_nr
-
-      # adding year to date
-      b$Date <- paste0(b$Date, "/", year)
-
-      # to avoid gtools::smartbind function or similar from another package..
-      if (ncol(data_station) >= ncol(b)) {
-        b[setdiff(names(data_station), names(b))] <- NA # adding missing columns
-        data_station <- rbind(data_station, b)  # joining data
-
-      } else { # when b have more columns then data_station
-       if(nrow(data_station) == 0){
-         data_station = b
+      # run only if downloaded file is valid
+      if(!is.na(file.size(temp)) & (file.size(temp) > 0)) { 
+        
+        a <- readHTMLTable(temp, stringsAsFactors = FALSE)
+        unlink(temp)
+        
+        b <- a[[length(a)]]
+        if (sum(b[1,]=="Dailyweather summary", na.rm = TRUE)) {
+          b <- b[,1:(length(b) - 8)]
         } else {
-          # adding missing columns
-          data_station <- merge(b,data_station, all = TRUE)# joining data
+          b <- b[, 1:length(b)]
         }
-
+        
+        test <- b[1:2, ]
+        if (is.null(test)) {
+          warning(paste0("Wrong station ID: ", station_nr, " You can check station ID at https://ogimet.com/display_stations.php?lang=en&tipo=AND&isyn=&oaci=&nombre=&estado=&Send=Send"))
+          return(data_station)
+          } 
+        #exeptions diferent names_col
+          if ((length(test[2, !is.na(test[2, ])]) == 6 &
+               test[2, 5] == "Int.")) {
+            names_col = unlist(c(
+              test[1, 1],
+              paste(test[1, 2], test[2, 1:3], sep = "_"),
+              test[1, 3:4],
+              paste(test[1, 5], test[2, 4:6], sep = "_"),
+              test[1, c(6:(length(test) - 4))]
+            ))
+          } else if ((length(test[2, !is.na(test[2, ])]) == 2 &
+                      test[2, 2] == "Int.")) {
+            names_col = unlist(c(test[1, 1:2], paste(test[1, 3], test[2, 1:2], sep = "_"), test[1, c(4:(length(test) - 1))]))
+          } else if ((length(test[2, !is.na(test[2, ])]) == 5 &
+                      test[2, 5] == "Int.")) {
+            names_col = unlist(c(
+              test[1, 1],
+              paste(test[1, 2], test[2, 1:3], sep = "_"),
+              test[1, 3:4],
+              paste(test[1, 5], test[2, 4:5], sep = "_"),
+              test[1, c(6:(length(test) - 3))]
+            ))
+          } else if ((length(test[2, !is.na(test[2, ])]) == 3 &
+                      test[2, 2] == "Int.")) {
+            names_col = unlist(c(
+              test[1, 1:2],
+              paste(test[1, 3], test[2, 1:3], sep = "_"),
+              test[1, c(4:(length(test) - 2))]
+            ))
+          }else {
+            names_col = "Error_column"
+          }
+        
+        
+        names_col <-
+          gsub("[^A-Za-z0-9]",
+               "",
+               as.character(lapply(names_col, as.character), stringsAsFactors = FALSE))
+  
+        colnames(b) <- names_col
+        b <- b[-c(1:2), ]
+        b["station_ID"] <- station_nr
+  
+        # adding year to date
+        b$Date <- as.character(paste0(b$Date, "/", year))
+  
+        # to avoid gtools::smartbind function or similar from another package..
+        if (ncol(data_station) >= ncol(b)) {
+          b[setdiff(names(data_station), names(b))] <- NA # adding missing columns
+          data_station <- rbind(data_station, b)  # joining data
+  
+        } else { # when b have more columns then data_station
+         if(nrow(data_station) == 0){
+           data_station = b
+          } else {
+            # adding missing columns
+            data_station <- merge(b, data_station, all = TRUE)# joining data
+          }
+  
+          }
+  
+        # cat(paste(year,month,"\n"))
+        # coords można lepiej na samym koncu dodać kolumne
+        # wtedy jak zmienia się lokalizacja na dacie to tutaj tez
+        if (coords){
+          coord <- a[[1]][2,1]
+          data_station["Lon"] <- get_coord_from_string(coord, "Longitude")
+          data_station["Lat"] <- get_coord_from_string(coord, "Latitude")
         }
+  
+  
+        } # end of checking for empty files / problems with connection
+      
+      } # end of looping for dates
 
-      # cat(paste(year,month,"\n"))
-      # coords można lepiej na samym koncu dodać kolumne
-      # wtedy jak zmienia się lokalizacja na dacie to tutaj tez
-      if (coords){
-        coord <- a[[1]][2,1]
-        data_station["Lon"] <- get_coord_from_string(coord, "Longitude")
-        data_station["Lat"] <- get_coord_from_string(coord, "Latitude")
-      }
-
-
-    } # koniec petli daty
-
-    data_station <-  data_station[!duplicated(data_station), ]
-
-
-  }# koniec petli stacje
-
-  # converting character to proper field representation:
-
-  # get rid off "---" standing for missing/blank fields:
-  data_station[which(data_station == "--" | data_station == "---" | data_station == "----" | data_station == "-----", arr.ind = TRUE)] <- NA
-
-  # other columns to numeric:
-  suppressWarnings(data_station[,c("TemperatureCMax", "TemperatureCMin", "TemperatureCAvg","TdAvgC" ,"HrAvg",
-                                   "WindkmhInt","WindkmhGust" ,"PresslevHp", "Precmm" ,
-                                   "TotClOct", "lowClOct" ,"VisKm","station_ID")] <-
-                     as.data.frame(sapply(data_station[,c("TemperatureCMax", "TemperatureCMin", "TemperatureCAvg","TdAvgC" ,"HrAvg",
-                                                          "WindkmhInt","WindkmhGust" ,"PresslevHp", "Precmm" ,
-                                                          "TotClOct", "lowClOct" ,"VisKm","station_ID")], as.numeric)))
-  # date to as.Date()
-  data_station$Date <- as.Date(data_station$Date, format = "%m/%d/%Y")
-
-  #  TODO:
-  # changing order of columns and removing blank records:
-  if(coords){
-    ord1 <- c("station_ID", "Lon", "Lat", "Date", "TemperatureCAvg")
-    ord1 <- c(ord1, setdiff(names(data_station), c("station_ID", "Lon", "Lat", "Date", "TemperatureCAvg")))
-    data_station <- data_station[, ord1]
-  } else {
-    ord1 <- c("station_ID", "Date", "TemperatureCAvg")
-    ord1 <- c(ord1, setdiff(names(data_station), c("station_ID", "Date", "TemperatureCAvg")))
-    data_station <- data_station[, ord1]
-  }
-  # setdiff(names(df), c("station_ID", "Date", "TC"))
-
-
-  # clipping to interesting period as we're downloading slightly more than needed:
-   data_station <- data_station[which(data_station$Date >= as.Date(min(date)) & as.Date(data_station$Date) <= as.Date(max(date))), ]
+    }# end of looping for stations
+    
+    if(nrow(data_station) > 0){
+      
+      data_station <-  data_station[!duplicated(data_station), ]
+      
+  
+    # converting character to proper field representation:
+  
+    # get rid off "---" standing for missing/blank fields:
+    data_station[which(data_station == "--" | data_station == "---" | data_station == "----" | data_station == "-----", arr.ind = TRUE)] <- NA
+  
+    # other columns to numeric:
+    suppressWarnings(data_station[,c("TemperatureCMax", "TemperatureCMin", "TemperatureCAvg","TdAvgC" ,"HrAvg",
+                                     "WindkmhInt","WindkmhGust" ,"PresslevHp", "Precmm" ,
+                                     "TotClOct", "lowClOct" ,"VisKm","station_ID")] <-
+                       as.data.frame(sapply(data_station[,c("TemperatureCMax", "TemperatureCMin", "TemperatureCAvg","TdAvgC" ,"HrAvg",
+                                                            "WindkmhInt","WindkmhGust" ,"PresslevHp", "Precmm" ,
+                                                            "TotClOct", "lowClOct" ,"VisKm","station_ID")], as.numeric)))
+    
+    #  TODO:
+    # changing order of columns and removing blank records:
+    if(coords){
+      ord1 <- c("station_ID", "Lon", "Lat", "Date", "TemperatureCAvg")
+      ord1 <- c(ord1, setdiff(names(data_station), c("station_ID", "Lon", "Lat", "Date", "TemperatureCAvg")))
+      data_station <- data_station[, ord1]
+    } else {
+      ord1 <- c("station_ID", "Date", "TemperatureCAvg")
+      ord1 <- c(ord1, setdiff(names(data_station), c("station_ID", "Date", "TemperatureCAvg")))
+      data_station <- data_station[, ord1]
+    }
+    # setdiff(names(df), c("station_ID", "Date", "TC"))
+    
+    # date to as.Date()
+    data_station$Date <- as.Date(data_station$Date, format = "%m/%d/%Y")
+    # clipping to interesting period as we're downloading slightly more than needed:
+    data_station <- data_station[which(data_station$Date >= as.Date(min(date)) & as.Date(data_station$Date) <= as.Date(max(date))), ]
+      
+  } # end of checking whether no. of rows > 0 
 
   return(data_station)
 
