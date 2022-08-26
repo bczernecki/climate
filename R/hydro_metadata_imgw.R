d9e6b01750cc12913004086241a9aa87501d9991
bczernecki <bczernecki@gmail.com>
2022-08-26 02:28:56 +0200

    hydro metadata fix

--- R/hydro_metadata_imgw.R
+++ R/hydro_metadata_imgw.R
@@ -4,6 +4,7 @@
 #' By default, the function returns a list or data frame for a selected subset
 #`
 #' @param interval temporal resolution of the data ("daily" , "monthly", or "semiannual_and_annual")
+#' @param allow_failure logical - whether to proceed or stop on failure. By default set to TRUE (i.e. don't stop on error). For debugging purposes change to FALSE
 #' @keywords internal
 #' @examples
 #' \donttest{
@@ -12,7 +13,22 @@
 #'   meta = climate:::hydro_metadata_imgw(interval = "semiannual_and_annual")
 #' }
 
-hydro_metadata_imgw = function(interval) {
+hydro_metadata_imgw = function(interval, allow_failure = TRUE) {
+  
+  if (allow_failure) {
+    tryCatch(hydro_metadata_imgw_bp(interval),
+             error = function(e){
+               message(paste("Problems with downloading data.",
+                             "Run function with argument allow_failure = FALSE",
+                             "to see more details"))})
+  } else {
+    hydro_metadata_imgw_bp(interval)
+  }
+}
+
+#' @keywords internal
+#' @noRd
+hydro_metadata_imgw_bp = function(interval, allow_failure = TRUE) {
 
   base_url = "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_hydrologiczne/"
 
--- tests/testthat/test-hydro_metadata_imgw.R
+++ tests/testthat/test-hydro_metadata_imgw.R
@@ -5,10 +5,12 @@ h_m <- hydro_metadata_imgw("monthly")
 h_a <- hydro_metadata_imgw("semiannual_and_annual")
 
 test_that("hydro-metadata works!", {
-  expect_equal(dim(h_d[[1]]), c(10, 1))
-  expect_equal(dim(h_d[[2]]), c(10, 1))
-  expect_equal(dim(h_m[[1]]), c(10, 1))
-  expect_equal(dim(h_a[[1]]), c(16, 1))
-  expect_equal(dim(h_a[[2]]), c(16, 1))
-  expect_equal(dim(h_a[[3]]), c(16, 1))
+  if (is.list(h_d) && is.list(h_m) && is.list(h_a)) {
+    expect_equal(dim(h_d[[1]]), c(10, 1))
+    expect_equal(dim(h_d[[2]]), c(10, 1))
+    expect_equal(dim(h_m[[1]]), c(10, 1))
+    expect_equal(dim(h_a[[1]]), c(16, 1))
+    expect_equal(dim(h_a[[2]]), c(16, 1))
+    expect_equal(dim(h_a[[3]]), c(16, 1))
+  }
 })
