8b2ede66752aa185b93c8193aeffd6c16a198551
MarcParm <47756519+MarcParm@users.noreply.github.com>
2023-01-11 12:18:02 +0100

    Update ogimet_daily.R
    I experienced a few (random) errors and was frustrated that I could not recover data already download before the error occurred. So I modified the code accordingly by using 'next' or 'break'.
    
    I also propose an improvement of the sequence of periods for a breakdown by month strictly (i.e. from 1 to 28, 30 or 31). In particular, an error is possible during a period straddling two years.

--- R/ogimet_daily.R
+++ R/ogimet_daily.R
@@ -46,8 +46,17 @@ ogimet_daily_bp = function(date = date,
                         coords = coords, 
                         station = station, 
                         hour = hour) {
-  dates = seq.Date(min(as.Date(date)), max(as.Date(date)), by = "1 month") 
-  dates = unique(c(dates, as.Date(max(date))))
+  
+  mindate = min(as.Date(date))
+  maxdate = max(as.Date(date))
+  # construct sequence of period (one period for each month)
+  # dates = end of month
+  dates = seq.Date(as.Date(format(mindate,"%Y/%m/01"),format="%Y/%m/%d"), 
+                   maxdate,
+                   by = "month") 
+  dates = dates - 1
+  dates = unique(c(dates, maxdate))
+  dates = dates[-1]
 
   # initalizing empty data frame for storing results:
   message(
@@ -88,6 +97,7 @@ ogimet_daily_bp = function(date = date,
     } 
 
     for (i in length(dates):1) {
+      
       # update progressbar:
       if (length(dates) >= 3) paste(setTxtProgressBar(pb, abs(length(dates)*length(station) - i)), "\n")
       
@@ -95,160 +105,162 @@ ogimet_daily_bp = function(date = date,
       month = format(dates[i], "%m")
       day = format(dates[i], "%d")
       ndays = day
-      linkpl2 = paste("https://www.ogimet.com/cgi-bin/gsynres?lang=en&ind=", station_nr, "&ndays=32&ano=", year, "&mes=", month, "&day=", day, "&hora=", hour,"&ord=REV&Send=Send", sep = "")
-      if (month == 1) linkpl2 = paste("https://www.ogimet.com/cgi-bin/gsynres?lang=en&ind=", station_nr, "&ndays=32&ano=", year, "&mes=", month, "&day=", day, "&hora=", hour, "&ord=REV&Send=Send", sep = "")
+      linkpl2 = paste("https://www.ogimet.com/cgi-bin/gsynres?lang=en&ind=", station_nr, "&ndays=",ndays,"&ano=", year, "&mes=", month, "&day=", day, "&hora=", hour,"&ord=REV&Send=Send", sep = "")
+     # if (month == 1) linkpl2 = paste("https://www.ogimet.com/cgi-bin/gsynres?lang=en&ind=", station_nr, "&ndays=32&ano=", year, "&mes=", month, "&day=", day, "&hora=", hour, "&ord=REV&Send=Send", sep = "")
       
       temp = tempfile()
       test_url(linkpl2, temp)
       
       # run only if downloaded file is valid
-      if (!is.na(file.size(temp)) & (file.size(temp) > 500)) { 
-        
-        a = readHTMLTable(temp, stringsAsFactors = FALSE)
+      if (is.na(file.size(temp)) | (file.size(temp) <= 500)) { 
         unlink(temp)
+        next # continue to next period
+      }
         
-        b = a[[length(a)]]
+      a = readHTMLTable(temp, stringsAsFactors = FALSE)
+      unlink(temp)
+      
+      b = a[[length(a)]]
+      
+      # check no data situations:
+      tst = sum(grepl(x = b[[1]], pattern = "No valid data"))
+      if (tst) {
+        message(paste(b[[1]], dates[i]))
+        next # continue to next period
+      } 
+      
+      if (sum(b[1, ] == "Dailyweather summary", na.rm = TRUE)) {
+        b = b[, 1:(length(b) - 8)]
+      } else {
+        b = b[, 1:length(b)]
+      }
+      
+      test = b[1:2, ]
+      
+      if (is.null(test) ) {
+        warning(paste0("Wrong station ID: ", station_nr, " You can check available stations ID at https://ogimet.com/display_stations.php?lang=en&tipo=AND&isyn=&oaci=&nombre=&estado=&Send=Send"))
+        break # continue with next iteration in the loop for station
+      } 
+      
+      # all columns are empty:
+      if (all(is.na(test[2, ]))) {
+        warning("no values in column names")
+        next # continue to next period
+      } 
         
-        # check no data situations:
-        tst = sum(grepl(x = b[[1]], pattern = "No valid data"))
-        if (tst) {
-          message(paste(b[[1]], dates[i]))
+        # number of columns contain weird/non-standard data (e.g. only wind speed)
+        if (ncol(test) <= 4) {
+          warning(paste0("Mandatory meteorological parameters (i.e. Temperature or precipitations) are not present. \nCheck content of the data using current URL:\n",
+                         linkpl2))
+          next # continue to next period
+        }
+        
+        if ((length(test[2, !is.na(test[2, ])]) == 6 &
+             test[2, 5] == "Int.")) {
+          names_col = unlist(c(
+            test[1, 1],
+            paste(test[1, 2], test[2, 1:3], sep = "_"),
+            test[1, 3:4],
+            paste(test[1, 5], test[2, 4:6], sep = "_"),
+            test[1, c(6:(length(test) - 4))]
+          ))
+        } else if ((length(test[2, !is.na(test[2, ])]) == 2 & test[2, 2] == "Int.")) {
+          names_col = unlist(c(test[1, 1:2], paste(test[1, 3], test[2, 1:2], sep = "_"), test[1, c(4:(length(test) - 1))]))
+        } else if ((length(test[2, !is.na(test[2, ])]) == 5 & test[2, 5] == "Int.")) {
+          names_col = unlist(c(
+            test[1, 1],
+            paste(test[1, 2], test[2, 1:3], sep = "_"),
+            test[1, 3:4],
+            paste(test[1, 5], test[2, 4:5], sep = "_"),
+            test[1, c(6:(length(test) - 3))]
+          ))
+        } else if ((length(test[2, !is.na(test[2, ])]) == 3 & test[2, 2] == "Int.")) {
+          names_col = unlist(c(
+            test[1, 1:2],
+            paste(test[1, 3], test[2, 1:3], sep = "_"),
+            test[1, c(4:(length(test) - 2))]
+          ))
         } else {
-        
-          if (sum(b[1, ] == "Dailyweather summary", na.rm = TRUE)) {
-            b = b[, 1:(length(b) - 8)]
-          } else {
-            b = b[, 1:length(b)]
-          }
-          
-          test = b[1:2, ]
-          
-          if (is.null(test) ) {
-            warning(paste0("Wrong station ID: ", station_nr, " You can check available stations ID at https://ogimet.com/display_stations.php?lang=en&tipo=AND&isyn=&oaci=&nombre=&estado=&Send=Send"))
-            return(data_station)
-          } 
-          
-          # all columns are empty:
-          if (all(is.na(test[2, ]))) {
-            message("no values in column names")
-          } else {
-            
-            # number of columns contain weird/non-standard data (e.g. only wind speed)
-            if (ncol(test) <= 4) {
-              stop(paste0("Mandatory meteorological parameters (i.e. Temperature or precipitations) are not present. \nCheck content of the data using current URL:\n",
-                             linkpl2))
-            }
-            
-            if ((length(test[2, !is.na(test[2, ])]) == 6 &
-                 test[2, 5] == "Int.")) {
-              names_col = unlist(c(
-                test[1, 1],
-                paste(test[1, 2], test[2, 1:3], sep = "_"),
-                test[1, 3:4],
-                paste(test[1, 5], test[2, 4:6], sep = "_"),
-                test[1, c(6:(length(test) - 4))]
-              ))
-            } else if ((length(test[2, !is.na(test[2, ])]) == 2 & test[2, 2] == "Int.")) {
-              names_col = unlist(c(test[1, 1:2], paste(test[1, 3], test[2, 1:2], sep = "_"), test[1, c(4:(length(test) - 1))]))
-            } else if ((length(test[2, !is.na(test[2, ])]) == 5 & test[2, 5] == "Int.")) {
-              names_col = unlist(c(
-                test[1, 1],
-                paste(test[1, 2], test[2, 1:3], sep = "_"),
-                test[1, 3:4],
-                paste(test[1, 5], test[2, 4:5], sep = "_"),
-                test[1, c(6:(length(test) - 3))]
-              ))
-            } else if ((length(test[2, !is.na(test[2, ])]) == 3 & test[2, 2] == "Int.")) {
-              names_col = unlist(c(
-                test[1, 1:2],
-                paste(test[1, 3], test[2, 1:3], sep = "_"),
-                test[1, c(4:(length(test) - 2))]
-              ))
-            } else {
-              names_col = "Error_column"
-            }
+          names_col = "Error_column"
+        }
 
-          names_col <-
-            gsub("[^A-Za-z0-9]",
-                 "",
-                 as.character(lapply(names_col, as.character), stringsAsFactors = FALSE))
-    
-          colnames(b) = names_col
-          b = b[-c(1:2), ]
-          b["station_ID"] = station_nr
-    
-          # adding year to date
-          b$Date = as.character(paste0(b$Date, "/", year))
-          
-          
-          # to avoid gtools::smartbind function or similar from another package..
-          if (ncol(data_station) >= ncol(b)) {
-            b[setdiff(names(data_station), names(b))] = NA # adding missing columns
-            data_station = rbind(data_station, b)  # joining data
-    
-          } else { # when b have more columns then data_station
-           if (nrow(data_station) == 0) {
-             data_station = b
-            } else {
-              # adding missing columns
-              data_station = merge(b, data_station, all = TRUE)# joining data
-            }
-    
-            }
-    
-          # cat(paste(year,month,"\n"))
-          # coords można lepiej na samym koncu dodać kolumne
-          # wtedy jak zmienia się lokalizacja na dacie to tutaj tez
-          if (coords) {
-            coord = a[[1]][2, 1]
-            data_station["Lon"] = get_coord_from_string(coord, "Longitude")
-            data_station["Lat"] = get_coord_from_string(coord, "Latitude")
-          }
-          
-              } # end of checking for: no values in column names
-    
-            } # end of checking for: No valid data found in database
-    
-          } # end of checking for empty files / problems with connection
+      names_col <-
+        gsub("[^A-Za-z0-9]",
+             "",
+             as.character(lapply(names_col, as.character), stringsAsFactors = FALSE))
+  
+      colnames(b) = names_col
+      b = b[-c(1:2), ]
+      b["station_ID"] = station_nr
+  
+      # adding year to date
+      b$Date = as.character(paste0(b$Date, "/", year))
+      # Carefull : mistake if period straddling two years!!!
+   
       
-      } # end of looping for dates
+      # to avoid gtools::smartbind function or similar from another package..
+      if (ncol(data_station) >= ncol(b)) {
+        b[setdiff(names(data_station), names(b))] = NA # adding missing columns
+        data_station = rbind(data_station, b)  # joining data
+      } else { # when b have more columns then data_station
+       if (nrow(data_station) == 0) {
+         data_station = b
+        } else {
+          # adding missing columns
+          data_station = merge(b, data_station, all = TRUE)# joining data
+        }
+      }
+  
+      # cat(paste(year,month,"\n"))
+      # coords można lepiej na samym koncu dodać kolumne
+      # wtedy jak zmienia się lokalizacja na dacie to tutaj tez
+      if (coords) {
+        coord = a[[1]][2, 1]
+        data_station["Lon"] = get_coord_from_string(coord, "Longitude")
+        data_station["Lat"] = get_coord_from_string(coord, "Latitude")
+      }
+      
+    } # end of looping for dates
 
-    }# end of looping for stations
+  }# end of looping for stations
     
-    if (nrow(data_station) > 0) {
-      
-      data_station =  data_station[!duplicated(data_station), ]
-      
-    # converting character to proper field representation:
-  
-    # get rid off "---" standing for missing/blank fields:
-    data_station[which(data_station == "--" | data_station == "---" | data_station == "----" | data_station == "-----", arr.ind = TRUE)] = NA
-  
-    # other columns to numeric:
-    suppressWarnings(data_station[,c("TemperatureCMax", "TemperatureCMin", "TemperatureCAvg","TdAvgC" ,"HrAvg",
-                                     "WindkmhInt","WindkmhGust" ,"PresslevHp", "Precmm" ,
-                                     "TotClOct", "lowClOct" ,"VisKm","station_ID")] <-
-                       as.data.frame(sapply(data_station[,c("TemperatureCMax", "TemperatureCMin", "TemperatureCAvg","TdAvgC" ,"HrAvg",
-                                                            "WindkmhInt","WindkmhGust" ,"PresslevHp", "Precmm" ,
-                                                            "TotClOct", "lowClOct" ,"VisKm","station_ID")], as.numeric)))
-    
-    # changing order of columns and removing blank records:
-    if (coords) {
-      ord1 = c("station_ID", "Lon", "Lat", "Date", "TemperatureCAvg")
-      ord1 = c(ord1, setdiff(names(data_station), c("station_ID", "Lon", "Lat", "Date", "TemperatureCAvg")))
-      data_station = data_station[, ord1]
-    } else {
-      ord1 = c("station_ID", "Date", "TemperatureCAvg")
-      ord1 = c(ord1, setdiff(names(data_station), c("station_ID", "Date", "TemperatureCAvg")))
-      data_station = data_station[, ord1]
-    }
-    
-    # date to as.Date()
-    data_station$Date = as.Date(as.character(data_station$Date), format = "%m/%d/%Y")
-    # clipping to interesting period as we're downloading slightly more than needed:
-    data_station = data_station[which(data_station$Date >= as.Date(min(date)) & as.Date(data_station$Date) <= as.Date(max(date))), ]
+  if (nrow(data_station) == 0) {
+        message("no data found")
+        return(0)
+  }
 
-  } # end of checking whether no. of rows > 0 
+    
+  data_station = data_station[!duplicated(data_station), ]
+    
+  # converting character to proper field representation:
   
+  # get rid off "---" standing for missing/blank fields:
+  data_station[which(data_station == "--" | data_station == "---" | data_station == "----" | data_station == "-----", arr.ind = TRUE)] = NA
+  
+  # other columns to numeric:
+  suppressWarnings(data_station[,c("TemperatureCMax", "TemperatureCMin", "TemperatureCAvg","TdAvgC" ,"HrAvg",
+                                   "WindkmhInt","WindkmhGust" ,"PresslevHp", "Precmm" ,
+                                   "TotClOct", "lowClOct" ,"VisKm","station_ID")] <-
+                     as.data.frame(sapply(data_station[,c("TemperatureCMax", "TemperatureCMin", "TemperatureCAvg","TdAvgC" ,"HrAvg",
+                                                          "WindkmhInt","WindkmhGust" ,"PresslevHp", "Precmm" ,
+                                                          "TotClOct", "lowClOct" ,"VisKm","station_ID")], as.numeric)))
+    
+  # changing order of columns and removing blank records:
+  if (coords) {
+    ord1 = c("station_ID", "Lon", "Lat", "Date", "TemperatureCAvg")
+    ord1 = c(ord1, setdiff(names(data_station), c("station_ID", "Lon", "Lat", "Date", "TemperatureCAvg")))
+    data_station = data_station[, ord1]
+  } else {
+    ord1 = c("station_ID", "Date", "TemperatureCAvg")
+    ord1 = c(ord1, setdiff(names(data_station), c("station_ID", "Date", "TemperatureCAvg")))
+    data_station = data_station[, ord1]
+  }
+    
+  # date to as.Date()
+  data_station$Date = as.Date(as.character(data_station$Date), format = "%m/%d/%Y")
+  
+  # clipping to interesting period as we're downloading slightly more than needed:
+  data_station = data_station[which(data_station$Date >= as.Date(min(date)) & as.Date(data_station$Date) <= as.Date(max(date))), ]
+
   return(data_station)
 }
