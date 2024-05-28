7cdd217f57ce90a16a144e2cd3116dad3a19febf
bczernecki <bczernecki@gmail.com>
2024-05-28 02:09:35 +0200

    fix: avoid warnings

--- R/meteo_imgw_monthly.R
+++ R/meteo_imgw_monthly.R
@@ -53,11 +53,6 @@ meteo_imgw_monthly = function(rank = "synop",
                                    station,
                                    col_names,
                                    ...),
-             warning = function(w) {
-               message(paste("Potential problem(s) found. Problems with downloading data.\n",
-                             "\rRun function with argument allow_failure = FALSE",
-                             "to see more details"))
-             },
              error = function(e){
                message(paste("Potential error(s) found. Problems with downloading data.\n",
                              "\rRun function with argument allow_failure = FALSE",
