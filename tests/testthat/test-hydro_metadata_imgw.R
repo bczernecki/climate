6f200cea21e678637e84bd32bd4d9229cae07a32
bczernecki <bczernecki@gmail.com>
2024-10-21 14:26:16 +0200

    fix: hydro test

--- tests/testthat/test-hydro_metadata_imgw.R
+++ tests/testthat/test-hydro_metadata_imgw.R
@@ -4,7 +4,7 @@ h_d <- suppressWarnings(hydro_metadata_imgw("daily"))
 h_m <- suppressWarnings(hydro_metadata_imgw("monthly"))
 
 test_that("hydro-metadata works!", {
-  if (is.list(h_d) && is.list(h_m) && is.list(h_a)) {
+  if (is.list(h_d) && is.list(h_m)) {
     expect_equal(dim(h_d[[1]]), c(10, 1))
     expect_equal(dim(h_d[[2]]), c(10, 1))
     expect_equal(dim(h_m[[1]]), c(10, 1))
