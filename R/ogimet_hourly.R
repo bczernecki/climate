21fea5ffd207022d5d27d2766eb907c3b378e3cb
bczernecki <bczernecki@gmail.com>
2025-10-29 12:52:20 +0100

    climate rc 1.2.5

--- R/ogimet_hourly.R
+++ R/ogimet_hourly.R
@@ -90,7 +90,7 @@ ogimet_hourly_bp = function(date = date,
     
     for (i in length(dates):1) {
       if (length(dates) >= 2 && i == length(dates)) {
-        msg = "  INFO: Please note that the Ogimet has recently limited number of queries that are accepted
+        msg = "\n INFO: Please note that the Ogimet has recently limited number of queries that are accepted
         \r by the server from a single IP address. Therefore, downloading more than approx. 1 month of data
         \r for a single station requires 20 seconds pause between subsequent queries and
         \r may take a while. Thank you for your patience."
