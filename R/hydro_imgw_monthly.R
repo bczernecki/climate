7f27aa126484378482dfa6b8ab7c0a08294799d4
bczernecki <bczernecki@gmail.com>
2026-03-04 22:55:07 +0100

    fix: hydro

--- R/hydro_imgw_monthly.R
+++ R/hydro_imgw_monthly.R
@@ -25,7 +25,7 @@
 hydro_imgw_monthly = function(year, 
                               coords = FALSE, 
                               station = NULL,
-                              col_names= "short", 
+                              col_names = "short", 
                               allow_failure = TRUE,
                               ...) {
   
@@ -86,7 +86,7 @@ hydro_imgw_monthly_bp = function(year,
     unzip(zipfile = temp, exdir = temp2)
     file1 = paste(temp2, dir(temp2), sep = "/")[1]
     data1 = imgw_read(translit, file1)
-    colnames(data1) = meta[[1]][, 1]
+    colnames(data1) = meta[, 1]
     all_data[[i]] = data1
   }
   all_data = do.call(rbind, all_data)
@@ -94,20 +94,20 @@ hydro_imgw_monthly_bp = function(year,
   all_data[all_data == 9999] = NA
   all_data[all_data == 99999.999] = NA
   all_data[all_data == 99.9] = NA
-  colnames(all_data) = meta[[1]][, 1]
+  colnames(all_data) = meta[, 1]
   # coords
   if (coords) {
-    all_data = merge(climate::imgw_hydro_stations, all_data, by.x = "id", by.y = "Kod stacji", all.y = TRUE)
+    all_data = merge(climate::imgw_hydro_stations, all_data, by.x = "id", by.y = "PSKDSZS", all.y = TRUE)
   }
   #station selection
   if (!is.null(station)) {
     if (is.character(station)) {
-      all_data = all_data[substr(all_data$`Nazwa stacji`, 1, nchar(station)) == station, ]
+      all_data = all_data[substr(all_data$PSNZWP, 1, nchar(station)) == station, ]
       if (nrow(all_data) == 0) {
         stop("Selected station(s) is not available in the database.", call. = FALSE)
       }
     } else if (is.numeric(station)) {
-      all_data = all_data[all_data$`Kod stacji` %in% station, ]
+      all_data = all_data[all_data$PSKDSZS %in% station, ]
       if (nrow(all_data) == 0) {
         stop("Selected station(s) is not available in the database.", call. = FALSE)
       }
@@ -115,17 +115,17 @@ hydro_imgw_monthly_bp = function(year,
       stop("Selected station(s) are not in the proper format.", call. = FALSE)
     }
   }
-  all_data = all_data[do.call(order, all_data[grep(x = colnames(all_data), "Nazwa stacji|Rok hydrologiczny|w roku hydro")]), ]
+  all_data = all_data[do.call(order, all_data[grep(x = colnames(all_data), "PSNZWP|MCROKH")]), ]
   # fix dates and add as seperate column:
-  yy_ind = grep(x = colnames(all_data), "Rok hydrologiczny")
-  mm_ind = grep(x = colnames(all_data), "kalendarzowy")
+  yy_ind = grep(x = colnames(all_data), "MCROKH")
+  mm_ind = grep(x = colnames(all_data), "MCMSCK")
   data_df = all_data[, c(yy_ind, mm_ind)]
   data_df$day = 1
   data_df$yy = ifelse(data_df[, 2] >= 11, data_df[, 1] - 1, data_df[, 1])
   all_data$Data = as.Date(ISOdate(year = data_df$yy, month = data_df[, 2], day = data_df$day))
   all_data = all_data[, c(1:3, ncol(all_data), 4:(ncol(all_data) - 1)), ]
 
-  all_data = hydro_shortening_imgw(all_data, col_names = col_names, ...)
+  #all_data = hydro_shortening_imgw(all_data, col_names = col_names, ...)
 
   return(all_data)
 }
